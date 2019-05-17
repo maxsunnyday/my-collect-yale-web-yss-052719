1891b2f16c6d5da090d235aeb9de90a4900170f6
Max Sun <max.sun@yale.edu>
2019-05-17 04:11:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,8 @@ def my_collect(array)
     new_array = []
     i = 0
     while i < array.length
-      new_array << yield array[i]
+      thing = yield array[i]
+      new_array << thing
       i += 1
     end
     new_array
