0daa3680a344ab10198a944ed653d4951a8fff3a
Max Sun <max.sun@yale.edu>
2019-05-17 04:11:41 +0000

    My collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  if block_given?
+    new_array = []
+    i = 0
+    while i < array.length
+      thing = yield array[i]
+      new_array << thing
+      i += 1
+    end
+    new_array
+  end
+end
