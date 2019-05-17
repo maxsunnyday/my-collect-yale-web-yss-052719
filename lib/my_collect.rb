5f73faf97a40f38f9c3035fca2aca0adb0db78e4
Max Sun <max.sun@yale.edu>
2019-05-17 04:10:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
 def my_collect(array)
   if block_given?
+    new_array = []
     i = 0
     while i < array.length
-      yield array[i]
+      new_array << yield array[i]
       i += 1
     end
-    array
+    new_array
   end
 end
